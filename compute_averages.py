3dd2043cf5580ce79ffded7e8d6506d739c3071f
olek <olekjanowiak916@gmail.com>
2026-01-07 22:56:54 +0100

    add new solution to Zad4

--- compute_averages.py
+++ compute_averages.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 def monthly_average(data, metadata_idx=3):
-    """
+    """(Zad2)
     Function used to compute monthly averages of PM2.5 concentration in Zad2.
     Averages over measurements in all stations for a given city in a given month (in a given year)
     Args:
@@ -10,7 +10,7 @@ def monthly_average(data, metadata_idx=3):
         metadata_idx (int): index of the first non-metadata row in the `data` DataFrame
 
     Returns:
-        result (pandas.DataFrame): a dataframe of average monthly PM2.5 in each city
+        result (pandas.DataFrame): a dataframe of average monthly PM2.5 in each city with MultiIndex (year, month) and cities as columns.
     """
     # add "year" an "month" columns for downstream indexing
     dt = pd.to_datetime(
@@ -22,7 +22,7 @@ def monthly_average(data, metadata_idx=3):
     data["year"] = dt.dt.year
     data["month"] = dt.dt.month
 
-    ## prepare the "city" column
+    ## prepare the new "city" column (used for aggregation)
     meta_cols = {"Miejscowość", "year", "month"}
     station_cols = [c for c in data.columns if c not in meta_cols]
 
@@ -35,7 +35,6 @@ def monthly_average(data, metadata_idx=3):
     station_to_city = dict(zip(station_cols, city_names))
 
     # drop metadata, then melt
-    metadata_idx = 3
     no_metadata_df = data[metadata_idx:].drop("Miejscowość", axis=1)
 
     long = no_metadata_df.melt(id_vars=["year", "month"], value_vars=station_cols, var_name="station", value_name="pm2.5")
@@ -48,7 +47,7 @@ def monthly_average(data, metadata_idx=3):
     long["pm2.5"] = pd.to_numeric(long["pm2.5"], errors="coerce")
     monthly_avg = long.groupby(["year", "month", "city"], as_index=False).mean(numeric_only=True)
 
-    # pivot back to a readable, wide format
+    # pivot back to a readable format
     result = (
         monthly_avg
         .pivot(
@@ -75,6 +74,80 @@ def monthly_average(data, metadata_idx=3):
     result.index = idx
 
     return result
+
+def count_days_over_treshold(data, treshold=15, metadata_idx=3):
+    """ (Zad4)
+    Function used to count days when PM2.5 concentration exceeds a given thershold for all stations in a given year.
+    Args:
+        data (pandas.DataFrame): a dataframe of PM2.5 levels
+        treshold (int): maximum acceptable PM2.5 according to WHO 
+        metadata_idx (int): index of the first non-metadata row in the `data` DataFrame
+
+    Returns:
+        exceedance_counts (pandas.DataFrame): a dataframe containing - for every station and every year - the number days where the average PM2.5 exceeded the acceptable thershold.
+    """
+    dt = pd.to_datetime(
+    data["Miejscowość"],
+    format="%Y-%m-%d %H:%M:%S",
+    errors="coerce"
+    )
+
+    data["date"] = dt.dt.date
+    data["year"] = dt.dt.year  
+
+    meta_cols = {"Miejscowość", "year", "date"}
+    station_cols = [c for c in data.columns if c not in meta_cols]
+
+    ## map station column name to city, then station code to city
+
+    # correct city names ("Kraków.1" to "Kraków")
+    station_to_city = (
+        pd.Series(station_cols, index=station_cols)
+        .str.replace(r"\.\d+$", "", regex=True)
+        .to_dict()
+    )
+
+    column_to_station = data.loc[0, station_cols].to_dict()
+
+    # drop metadata
+    df = data.iloc[metadata_idx:].copy()
+
+    # prepare the long dataframe used for aggregation
+    long = df.melt(
+        id_vars=["year", "date"],
+        value_vars=station_cols,
+        var_name="column",
+        value_name="pm25"
+    )
+
+    # eg. "station" is "MpKrakBulwar", "column" is "Kraków.1" and city is "Kraków" 
+    long["station"] = long["column"].map(column_to_station)
+    long["city"] = long["column"].map(station_to_city)
+
+    long = long.drop("column", axis=1) # get rid of the intermediate column with station column names (like "Kraków.1")
+
+    long["pm25"] = pd.to_numeric(long["pm25"], errors="coerce")
+
+    # compute daily average PM2.5 by station
+    daily  = (
+        long
+        .groupby(["year", "date", "city", "station"], as_index=False)
+        .agg(daily_pm25=("pm25", "mean"))
+    )
+
+    daily["exceeded"] = daily["daily_pm25"] > treshold
+
+    exceedance_counts = (
+        daily
+        .groupby(["year", "city", "station"], as_index=False)
+        .agg(days_exceeded=("exceeded", "sum"))
+    )
+
+    # convert "year" from float to int
+    exceedance_counts["year"] = exceedance_counts["year"].astype('int')
+
+    return exceedance_counts
+
 
 def main():
     print("compute_averages module. This is only to be used through an import.")
--- visualizations.py
+++ visualizations.py
@@ -3,12 +3,12 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def plot_city_pm25_trends(monthly_df, cities=["Warszawa", "Katowice"], years=[2015, 2024], ylim=[0, 75]):
-    """
+def plot_city_trends(monthly_df, cities=["Warszawa", "Katowice"], years=[2015, 2024], ylim=[0, 75]):
+    """(Zad2)
     Plot monthly PM2.5 trends for selected cities and years.
 
     Args:
-        result (pd.DataFrame): DataFrame with MultiIndex (year, month) and cities as columns.
+        monthly_df (pd.DataFrame): DataFrame with MultiIndex (year, month) and cities as columns.
         cities (list[str]): Cities to include in the plot.
         years (list[int]): Years to include in the plot.
         ylim (list[int]): Y-axis limits.
@@ -71,6 +71,99 @@ def plot_city_pm25_trends(monthly_df, cities=["Warszawa", "Katowice"], years=[20
     ax.legend(title="", frameon=False)
     fig.tight_layout()
 
+    return ax
+
+
+def plot_pm25_exceedance_bars(
+    exceedance_counts: pd.DataFrame,
+    top_n: int = 3,
+    base_year: int = 2024,
+    threshold: float = 15,
+    figsize=(12, 6),
+):
+    """(Zad4)
+    Create a grouped barplot of the number of days with average PM2.5 above who_threshold.
+    Include top_n best and worst stations in terms of days over treshold in year base_year.
+
+    Args:
+        exceedance_counts (pd.DataFrame): 
+        top_n (int): Number of highest and lowest exceedence stations to be displayed.
+        base_year (int): year that constitues the criterion for selecting the highest and lowest exceedence stations.  
+        who_threshold (int): information to be displayed on the plot
+
+    Returns:
+        ax (matplotlib.axes.Axes): the desired barplot to be shown in Zad4
+    """
+    # select top & bottom stations based on base_year
+    exceedance_base = (
+        exceedance_counts
+        .loc[exceedance_counts["year"] == base_year]
+        .sort_values("days_exceeded", ascending=True)
+    )
+
+    top_stations = exceedance_base["station"].tail(top_n).tolist()
+    bottom_stations = exceedance_base["station"].head(top_n).tolist()
+    # keep selected stations in a list for plot ordering
+    selected_stations = bottom_stations + top_stations
+
+    plot_df = (
+        exceedance_counts
+        .loc[exceedance_counts["station"].isin(selected_stations)]
+        .copy()
+    )
+
+    # create labels for the barplot
+    plot_df["label"] = (
+        plot_df["city"].astype(str) + ": " + plot_df["station"].astype(str)
+    )
+
+    # build station to label mapping to make labels ordered correctly
+    label_map = (
+        plot_df
+        .drop_duplicates("station")
+        .set_index("station")["label"]
+        .to_dict()
+    )
+
+    # try to make a colormap that always looks good...
+    years = sorted(plot_df["year"].unique())
+    palette = sns.color_palette("magma", n_colors=len(years) + 2)[2:]
+    year_palette = dict(zip(years, palette))
+
+    fig, ax = plt.subplots(figsize=figsize)
+
+    sns.barplot(
+        data=plot_df,
+        x="station",
+        y="days_exceeded",
+        hue="year",
+        order=selected_stations,
+        palette=year_palette,
+        ax=ax,
+    )
+
+    ax.set_xticks(range(len(selected_stations)))
+    # replace x-axis labels with "city: station"
+    ax.set_xticklabels(
+        [label_map[s] for s in selected_stations],
+        rotation=45,
+        ha="right"
+    )
+
+    # styling
+    ax.set_title(
+        "Liczba dni z przekroczeniem normy dobowej PM2.5\n"
+        f"(PM2.5 > {threshold} µg/m³, WHO)\n"
+        f"{top_n} stacje z najmniejszą i {top_n} z największą liczbą dni w {base_year}",
+        fontsize=13,
+    )
+
+    ax.set_xlabel("Stacja pomiarowa (miasto: kod stacji)")
+    ax.set_ylabel("Liczba dni z przekroczeniem normy dobowej")
+    ax.grid(axis="y", alpha=0.4)
+    ax.legend(title="Rok", frameon=False)
+
+    fig.tight_layout()
     return ax
 
 def main():
