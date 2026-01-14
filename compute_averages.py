63bc844ebbeb9a09f6d6f530c58978d35fc23d5a
Dominika <dominika.aszewska@gmail.com>
2026-01-14 23:42:48 +0100

    dodanie zad 3 do visualizations i main.ipynb

--- compute_averages.py
+++ compute_averages.py
@@ -1,131 +1,111 @@
 # liczenie średnich i wskazywanie dni z przekroczeniem normy
 import pandas as pd
-import load_data as load_data
 
-def monthly_average(data, metadata_idx=3):
+def monthly_average(data):
     """(Zad2)
     Function used to compute monthly averages of PM2.5 concentration in Zad2.
     Averages over measurements in all stations for a given city in a given month (in a given year)
     Args:
         data (pandas.DataFrame): a dataframe of PM2.5 levels
-        metadata_idx (int): index of the first non-metadata row in the `data` DataFrame
 
     Returns:
         result (pandas.DataFrame): a dataframe of average monthly PM2.5 in each city with MultiIndex (year, month) and cities as columns.
     """
-    # add "year" an "month" columns for downstream indexing
-    dt = pd.to_datetime(
-        data["Miejscowość"],
-        format="%Y-%m-%d %H:%M:%S",
-        errors="coerce"
-    )
+    ## convert the incoherent date column to one unified format
+    s = data["Kod stacji"].astype(str)
 
+    # first attempt: with milliseconds
+    dt = pd.to_datetime(s, format="%Y-%m-%d %H:%M:%S.%f", errors="coerce")
+
+    # second attempt: without milliseconds, only where first failed
+    mask = dt.isna()
+    dt[mask] = pd.to_datetime(s[mask], format="%Y-%m-%d %H:%M:%S", errors="coerce")
+
+    # assign back
+    data["Kod stacji"] = dt
+
+    # extract year and month
     data["year"] = dt.dt.year
     data["month"] = dt.dt.month
 
-    ## prepare the new "city" column (used for aggregation)
-    meta_cols = {"Miejscowość", "year", "month"}
+    data.drop("Rok", axis=1, inplace=True)
+
+    meta_cols = {"Kod stacji", "year", "month"}
     station_cols = [c for c in data.columns if c not in meta_cols]
 
-    # construct a dictionary for mapping stations to cities
-    city_names = (
-        pd.Series(station_cols)
-        .str.replace(r"\.\d+$", "", regex=True) # this converts eg. "Kraków.1" to "Kraków"
-    )
-
-    station_to_city = dict(zip(station_cols, city_names))
-
-    # drop metadata, then melt
-    no_metadata_df = data[metadata_idx:].drop("Miejscowość", axis=1)
+    no_metadata_df = data.drop("Kod stacji", axis=1)
 
     long = no_metadata_df.melt(id_vars=["year", "month"], value_vars=station_cols, var_name="station", value_name="pm2.5")
+    #long["station"].iloc[3]
 
-    # ensure all stations are assigned to a city
-    long["city"] = long["station"].map(station_to_city)
+    long["city"] = long["station"].str.extract(r"'([^']+)'")
     long.drop("station", axis=1, inplace=True)
 
     ## perform the actual aggregation
     long["pm2.5"] = pd.to_numeric(long["pm2.5"], errors="coerce")
-    monthly_avg = long.groupby(["year", "month", "city"], as_index=False).mean(numeric_only=True)
-
-    # pivot back to a readable format
-    result = (
-        monthly_avg
-        .pivot(
-            index=["year", "month"],
-            columns="city",
-            values="pm2.5"
-        )
-        .sort_index()
+    long_avg = long.groupby(["year", "month", "city"], as_index=False).mean(numeric_only=True)
+    monthly = (long_avg
+    .pivot(
+        index=["year", "month"],
+        columns="city",
+        values="pm2.5"
     )
+    .sort_index())
 
-    # convert Multindex from float to int
-    idx = result.index
-
-    idx = idx.set_levels(
-        idx.levels[idx.names.index("year")].astype(int),
-        level="year"
-    )
-
-    idx = idx.set_levels(
-        idx.levels[idx.names.index("month")].astype(int),
-        level="month"
-    )
-
-    result.index = idx
-
-    return result
-
-def count_days_over_treshold(data, treshold=15, metadata_idx=3):
+    return monthly
+    
+def count_days_over_treshold(data, treshold=15):
     """ (Zad4)
     Function used to count days when PM2.5 concentration exceeds a given thershold for all stations in a given year.
     Args:
         data (pandas.DataFrame): a dataframe of PM2.5 levels
         treshold (int): maximum acceptable PM2.5 according to WHO 
-        metadata_idx (int): index of the first non-metadata row in the `data` DataFrame
-
+      
     Returns:
         exceedance_counts (pandas.DataFrame): a dataframe containing - for every station and every year - the number days where the average PM2.5 exceeded the acceptable thershold.
     """
-    dt = pd.to_datetime(
-    data["Miejscowość"],
-    format="%Y-%m-%d %H:%M:%S",
-    errors="coerce"
-    )
+    ## convert the incoherent date column to one unified format
+    s = data["Kod stacji"].astype(str)
 
+    # first attempt: with milliseconds
+    dt = pd.to_datetime(s, format="%Y-%m-%d %H:%M:%S.%f", errors="coerce")
+
+    # second attempt: without milliseconds, only where first failed
+    mask = dt.isna()
+    dt[mask] = pd.to_datetime(s[mask], format="%Y-%m-%d %H:%M:%S", errors="coerce")
+
+    # assign back
+    data["Kod stacji"] = dt
+
+    # extract year and month
+    data["year"] = dt.dt.year
     data["date"] = dt.dt.date
-    data["year"] = dt.dt.year  
 
-    meta_cols = {"Miejscowość", "year", "date"}
+    data = data.drop("Rok", axis=1)
+
+    meta_cols = {"Kod stacji", "year", "month"}
     station_cols = [c for c in data.columns if c not in meta_cols]
 
-    ## map station column name to city, then station code to city
-
-    # correct city names ("Kraków.1" to "Kraków")
-    station_to_city = (
-        pd.Series(station_cols, index=station_cols)
-        .str.replace(r"\.\d+$", "", regex=True)
-        .to_dict()
-    )
-
-    column_to_station = data.loc[0, station_cols].to_dict()
-
-    # drop metadata
-    df = data.iloc[metadata_idx:].copy()
+    no_metadata_df = data.drop("Kod stacji", axis=1)
 
     # prepare the long dataframe used for aggregation
-    long = df.melt(
+    long = no_metadata_df.melt(
         id_vars=["year", "date"],
         value_vars=station_cols,
-        var_name="column",
-        value_name="pm25"
+        var_name="station_tuple",
+        value_name='pm25'
     )
 
-    # eg. "station" is "MpKrakBulwar", "column" is "Kraków.1" and city is "Kraków" 
-    long["station"] = long["column"].map(column_to_station)
-    long["city"] = long["column"].map(station_to_city)
+    # decouple city name and station code
 
-    long = long.drop("column", axis=1) # get rid of the intermediate column with station column names (like "Kraków.1")
+    long["station_tuple"] = long["station_tuple"].astype(str)
+
+    # extract city and station code into two new columns
+    long[["city", "station"]] = long["station_tuple"].str.extract(
+        r"\(\s*'([^']+)'\s*,\s*'([^']+)'\s*\)"
+    )
+
+    long.drop("station_tuple", axis=1)
 
     long["pm25"] = pd.to_numeric(long["pm25"], errors="coerce")
 
@@ -144,11 +124,7 @@ def count_days_over_treshold(data, treshold=15, metadata_idx=3):
         .agg(days_exceeded=("exceeded", "sum"))
     )
 
-    # convert "year" from float to int
-    exceedance_counts["year"] = exceedance_counts["year"].astype('int')
-
     return exceedance_counts
-
 
 def main():
     print("compute_averages module. This is only to be used through an import.")
--- visualizations.py
+++ visualizations.py
@@ -5,8 +5,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-from load_data import load_data
-from compute_averages import monthly_average
 
 def plot_city_trends(monthly_df, cities=["Warszawa", "Katowice"], years=[2015, 2024], ylim=[0, 75]):
     """(Zad2)
@@ -78,60 +76,63 @@ def plot_city_trends(monthly_df, cities=["Warszawa", "Katowice"], years=[2015, 2
 
     return ax
 
+def heatmaps(monthly_df):
 
-def heatmaps():
+    locations = [c for c in monthly_df.columns if c not in ["year", "month"]]
 
-    data = load_data()
-    monthly = monthly_average(data)
-    locations = monthly['location'].unique()
-    zmin = monthly['pm2.5'].min()
-    zmax = monthly['pm2.5'].max()
-    years = [2015, 2018, 2019, 2024]
+    zmin = monthly_df[locations].min().min()
+    zmax = monthly_df[locations].max().max()
+
+    years = [2015, 2018, 2021, 2024]
 
     n = len(locations)
     rows = int(np.ceil(n / 2))
     cols = 2
 
-    fig = make_subplots(rows=rows, cols=cols, subplot_titles=[f"{loc}" for loc in locations])
+    fig = make_subplots(rows=rows, cols=cols, subplot_titles=locations)
+
     colorscale = "Viridis"
 
     for i, loc in enumerate(locations):
         row = i // 2 + 1
         col = i % 2 + 1
 
-        dfloc = monthly[(monthly['location'] == loc) & (monthly['year'].astype(int).isin(years))]
+        dfloc = monthly_df[monthly_df["year"].isin(years)]
 
-        heatmap_data = dfloc.pivot(index='year', columns='month', values='pm25')
-        y = heatmap_data.index.astype(str)[::-1]
+        heatmap_data = dfloc.pivot(index="year", columns="month", values=loc)
 
-        showscale = True if i == 0 else False
+        heatmap_data = heatmap_data.reindex(index=years, columns=range(1, 13))
 
-        hm = go.Heatmap(z=heatmap_data.values[::-1, :], x=heatmap_data.columns, y=y,
+        y = heatmap_data.index.astype(str)
+        x = heatmap_data.columns
+
+        showscale = i == 0
+
+        hm = go.Heatmap(z=heatmap_data.values, x=x, y=y,
                         colorscale=colorscale,
                         zmin=zmin, zmax=zmax,
                         colorbar=dict(title=dict(text="PM2.5 µg/m³"),
-                                    tickmode="array",
-                                    tickvals=np.linspace(zmin, zmax, 5),
-                                    ticktext=[f"{v:.0f}" for v in np.linspace(zmin, zmax, 5)],
-                                    len=0.2,
-                                    y=0.7,
-                                    x=1.05),
-                        hovertemplate="Rok: %{y}<br>Miesiąc: %{x}<br>PM2.5: %{z} µg/m³",
+                                      tickmode="array",
+                                      tickvals=np.linspace(zmin, zmax, 5),
+                                      ticktext=[f"{v:.0f}" for v in np.linspace(zmin, zmax, 5)],
+                                      len=0.2, y=0.7, x=1.05),
+                        hovertemplate="PM2.5: %{z} µg/m³",
                         showscale=showscale)
 
         fig.add_trace(hm, row=row, col=col)
 
-    fig.update_xaxes(tickmode="array", tickvals=list(range(1, 13)), ticktext=list(range(1, 13)))
+    for i in range(1, rows * cols + 1):
+        fig.update_yaxes(categoryorder='array', categoryarray=y,
+                         row=(i - 1) // 2 + 1, col=(i - 1) % 2 + 1,
+                         title_text="Rok", title_standoff=1)
 
-    for i in range(1, rows*cols + 1):
-        fig.update_yaxes(title_text="Rok",
-                        categoryorder='array',
-                        categoryarray=[str(y) for y in years],
-                        autorange="reversed",
-                        row=(i-1) // 2 + 1,
-                        col=(i-1) % 2 + 1)
+        fig.update_xaxes(tickmode="array", tickvals=list(range(1, 13)), ticktext=list(range(1, 13)),
+                         row=(i - 1) // 2 + 1, col=(i - 1) % 2 + 1,
+                         title_text="Miesiąc", title_standoff=5)
 
-    fig.update_layout(height=350 * rows, width=1000, title=dict(text='Średnie PM2.5 w latach 2015, 2018, 2021 i 2024', x=0.5, y=0.99), font=dict(size=12))
+    fig.update_layout(height=250 * rows, width=750,
+                      title=dict(text='Średnie PM2.5 w latach 2015, 2018, 2021 i 2024', x=0.5, y=0.99),
+                      font=dict(size=11))
 
     return fig
 
