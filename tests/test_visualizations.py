d2c31cc8be0998c18abba99d6f6dc39b057f2e01
Dominika <dominikaaszewska@gmail.com>
2026-01-19 19:40:26 +0100

    Update test_visualizations.py

--- tests/test_visualizations.py
+++ tests/test_visualizations.py
@@ -12,4 +12,4 @@ def test_heatmap_run_without_err(monthly_df):
    assert fig is not None
 
 def test_city_trends_run_without_err(monthly_df, cities=["Warszawa", "Katowice"], years=[2015, 2024], ylim=[0, 75]):
-   assert plot_city_trends(monthly_df, cities=["Warszawa", "Katowice"], years=[2015, 2024], ylim=[0, 75])) is not None
+   assert plot_city_trends(monthly_df, cities=["Warszawa", "Katowice"], years=[2015, 2024], ylim=[0, 75]) is not None
