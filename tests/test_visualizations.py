3a5d413dc3005cbaec145c42bc01f2bae83437c8
Dominika <dominikaaszewska@gmail.com>
2026-01-19 20:24:26 +0100

    Update test_visualizations.py

--- tests/test_visualizations.py
+++ tests/test_visualizations.py
@@ -5,11 +5,13 @@ import pandas as pd
 
 @pytest.fixture(scope="session")
 def monthly_df():
-   return pd.DataFrame({"year": [2015, 2018, 2021, 2024],
+   df = pd.DataFrame({"year": [2015, 2018, 2021, 2024],
                        "month": [1, 2, 3, 4],
                        "Warszawa": [1.3, 20, 30, 5],
                         "Katowice": [4.2, 15, 3.1, 10],
                         "Lublin": [3.4, 8.1, 1.2, 2]})
+   df["year"] = df["year"].astype("int32")
+   return df
 
 def test_heatmap_run_without_err(monthly_df):
    fig = heatmaps(monthly_df)
