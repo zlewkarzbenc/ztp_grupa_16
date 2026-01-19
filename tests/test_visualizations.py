14f38865fdfac7a679be44e219f1bc30a82512bf
Dominika <dominika.aszewska@gmail.com>
2026-01-19 14:28:13 +0100

    testy do zad 2 i zad 4

--- tests/test_visualizations.py
+++ tests/test_visualizations.py
@@ -1,14 +1,34 @@
 from visualizations import *
 import pytest
+from compute_averages import *
 
 @pytest.fixture
 def monthly_df():
    monthly_df = pd.read_csv("monthly_average.csv")
    return monthly_df
 
+@pytest.fixture
+def data():
+    data = pd.read_csv("all_data.csv")
+    return data
+
 def test_city_trends_run_without_err(monthly_df):
-    fig, ax = plot_city_trends(monthly_df, cities=["Warszawa", "Katowice"], years=[2015, 2024], ylim=[0, 75])
+    fig, ax = plot_city_trends(monthly_df, years=[2015, 2024])
     assert fig is not None
+
+def test_city_trends_correct_data(monthly_df):
+    ax = plot_city_trends(monthly_df, years=[2015, 2024])
+
+    labels = [t.get_text() for t in ax.get_legend().get_texts()]
+    lines = ax.get_lines()
+
+    lines_map = {label: line for label, line in zip(labels, lines)}
+
+    assert np.all(lines_map["Warszawa 2015"].get_ydata() == monthly_df.loc[2015, "Warszawa"].to_numpy())
+    assert np.all(lines_map["Warszawa 2024"].get_ydata() == monthly_df.loc[2024, "Warszawa"].to_numpy())
+    assert np.all(lines_map["Katowice 2015"].get_ydata() == monthly_df.loc[2015, "Katowice"].to_numpy())
+    assert np.all(lines_map["Katowice 2024"].get_ydata() == monthly_df.loc[2024, "Katowice"].to_numpy())
+
 
 def test_heatmap_run_without_err(monthly_df):
     fig = heatmaps(monthly_df)
@@ -19,6 +39,24 @@ def test_heatmap_if_every_location(monthly_df):
     locations = [c for c in monthly_df.columns if c not in ["year", "month"]]
     assert len(fig.data) == len(locations)
 
-def test_pm25_exceedance_run_without_err(monthly_df):
-    fig = plot_pm25_exceedance_bars(monthly_df, top_n=3,  base_year=2024, threshold=15, figsize=(12, 6))
+def test_pm25_exceedance_run_without_err(data):
+    counts = count_days_over_treshold(data)
+    fig = plot_pm25_exceedance_bars(counts)
     assert fig is not None
+
+def test_pm25_exceedance_correct_data(data):
+    counts = count_days_over_treshold(data)
+
+    ax = plot_pm25_exceedance_bars(counts)
+
+    heights = [p.get_height() for p in ax.patches]
+
+    exceedance_base = (counts[counts["year"] == 2024].sort_values("days_exceeded", ascending=True))
+
+    top_stations = exceedance_base["station"].tail(3).tolist()
+    bottom_stations = exceedance_base["station"].head(3).tolist()
+    selected_stations = bottom_stations + top_stations
+
+    expected = (counts[counts["station"].isin(selected_stations)].sort_values(["station", "year"])["days_exceeded"].tolist())
+
+    assert heights == expected
