04ab3e3798c1717a5f17cbe45aa51cb78eeb94fd
Dominika <dominikaaszewska@gmail.com>
2026-01-19 18:35:31 +0100

    Update test_visualizations.py

--- tests/test_visualizations.py
+++ tests/test_visualizations.py
@@ -5,11 +5,7 @@ import pandas as pd
 
 @pytest.fixture(scope="session")
 def monthly_df():
-   return pd.DataFrame({"year": [2015, 2018, 2021],
-                        "month": [1, 2, 3],
-                        "Warszawa": [1.3, 20, 30],
-                        "Kraków": [4.2, 15, 3.1],
-                        "Lublin": [3.4, 8.1, 1.2]})
+   return pd.read_csv("monthly_average")
 
 def test_heatmap_run_without_err(monthly_df):
    fig = heatmaps(monthly_df)
