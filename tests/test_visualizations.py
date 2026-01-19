d60801b27bcf0d5ca268352285839ff9a03e0dc8
Dominika <dominikaaszewska@gmail.com>
2026-01-19 19:07:52 +0100

    Update test_visualizations.py

--- tests/test_visualizations.py
+++ tests/test_visualizations.py
@@ -10,3 +10,6 @@ def monthly_df():
 def test_heatmap_run_without_err(monthly_df):
    fig = heatmaps(monthly_df)
    assert fig is not None
+
+def test_city_trends_run_without_err(monthly_df):
+   assert plot_city_trends(monthly_df) is not None
